d99fc8f3184e6d5d9f633f9a5923f54ee68a4732
Nicolas Cordoba <nico343@outlook.es>
2025-02-13 16:13:42 -0300

    obtained all tournaments of a year

--- main.py
+++ main.py
@@ -7,11 +7,27 @@ def scrapp_padel_data(page: Page, url: str):
     page.wait_for_selector("section#event-data")
     events_section = page.locator("section#event-data")
     events = events_section.locator("section.month-section")
-    
+    dict_events = []
     for i in range(events.count()):
         # print(f"Event {i}")
         event = events.nth(i)
-        print(event.get_attribute("id"))
+        event_content = event.locator("div.event-content-wrap")
+        for i in range(event_content.count()):
+            tournament = event_content.nth(i)
+            link = tournament.locator("div.event-title").locator("a").get_attribute("href")
+            title = tournament.locator("div.event-title").inner_text()
+            date = tournament.locator("div.date-start-end").inner_text()
+            location = tournament.locator("div.event-location").inner_text()
+            tournament_dict = {
+                "link": link,
+                "title": title,
+                "date_start": date.split(' ')[1],
+                "date_end": date.split(' ')[-1],
+                "location": location}
+            dict_events.append(tournament_dict)
+                
+        # link = event_content.locator("div.event-title").locator("a").get_attribute("href")
+        # print(link)
 
 
 def run(playwright: Playwright, url: str):
