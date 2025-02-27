b8a5487f06ab8342226d19e62a78514fe45781b4
Nicolas Cordoba <nico343@outlook.es>
2025-02-27 12:52:09 -0300

    round categories added, fixed player names scrap.

--- plugins/scrap/scrap.py
+++ plugins/scrap/scrap.py
@@ -5,7 +5,7 @@ import datetime
 # url = "https://www.padelfip.com/es/calendario-premier-padel/?events-year=2024"
 
 
-def winner_calculation(team_1_scores: tuple, team_2_scores: tuple, dict_flag: dict = None) -> str:
+def winner_calculation(date,team_1,team_1_scores: tuple, team_2_scores: tuple, dict_flag: dict = None) -> str:
     if dict_flag:
         if dict_flag["flag"] == "WO":
             return dict_flag["team"]
@@ -13,11 +13,14 @@ def winner_calculation(team_1_scores: tuple, team_2_scores: tuple, dict_flag: di
             return "team_2" if dict_flag["team"] == "team_1" else "team_1"
         
     else:
-        t1_total_score = sum(
-            [int(i[0]) for i in team_1_scores if i != "-"])
-        t2_total_score = sum(
-            [int(i[0]) for i in team_2_scores if i != "-"])
-        return "team_1" if t1_total_score > t2_total_score else "team_2"
+        sets_1 = 0
+        sets_2 = 0
+        for i in range(3):
+            if team_1_scores[i].isnumeric():
+                if team_1_scores[i] > team_2_scores[i]: sets_1+=1
+                else: sets_2 +=1
+
+        return "team_1" if sets_1 > sets_2 else "team_2"
     
 
 def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
@@ -27,13 +30,13 @@ def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
     
     
     if "P2" in tournament_dict['title']:
-        rounds = ['Round of 32', 'Round of 16', 'Quarterfinals', 'Semifinals', 'Final']
+        rounds = ['Q1', 'Q2', 'Q3', 'Round of 32', 'Round of 16', 'Quarterfinals', 'Semifinals', 'Final']
         
     elif "FINALS" in tournament_dict['title']:
-        rounds = ['Quarterfinals', 'Semifinals', 'Final']
+        rounds = ['Quarterfinals', 'Semifinals', 'Final 3rd place', 'Final']
         
     else:
-        rounds = ['Round of 64', 'Round of 32', 'Round of 16', 'Quarterfinals', 'Semifinals', 'Final']
+        rounds = ['Q1', 'Q2', 'Round of 64', 'Round of 32', 'Round of 16', 'Quarterfinals', 'Semifinals', 'Final']
         
     # rounds_frame = page.locator("div.colDraw.trentaduesimidifinale")
     page.goto(url)
@@ -46,9 +49,9 @@ def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
     new_link = src[:-len(days + '?t=tol')]
     
     for k in range(int(days)):
-        day = k+1
-        date = date_start + datetime.timedelta(days=day)
-        page.goto(new_link + f"{day}?t=tol")
+        number_page = k+1
+        date = date_start + datetime.timedelta(days=k)
+        page.goto(new_link + f"{number_page}?t=tol")
         page.wait_for_selector("div#container")
         game_div = page.locator("div.col-12.p-0")
         
@@ -58,15 +61,15 @@ def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
             round_name_div = score_box.locator("div.round-name.text-right")
             # Raw gender and round name
             text_content = round_name_div.text_content()
-            # print(text_content)
+            
             # Extracter re pattern for gender and round names
             pattern_round_name = r"^\s*(\S.*\S)\s*$"
             gender, round_name = re.findall(pattern_round_name, text_content, re.MULTILINE)
-            round_id_dict = {"Q1":"Q1", "Q2": "Q2",'Round of 64':64, 'Round of 32':32, 'Round of 16':16, 'Quarterfinals':8, 'Semifinals':4, 'Final':2}
+            round_id_dict = {"Q1":"Q1", "Q2": "Q2", "Q3": 'Q3', 'Round of 64':64, 'Round of 32':32, 'Round of 16':16, 'Quarterfinals':8, 'Semifinals':4, 'Final 3rd place': 3, 'Final':2}
             
             if round_name in rounds and gender == gender_arg:
                 # Extracter re pattern for team names and scores
-                pattern_names = r"([A-Z]\.\s[A-Za-z]+(?:\s\(\d+\))?)"
+                pattern_names = r"([A-ZÁÉÍÓÚ]\.\s[A-Za-zÁÉÍÓÚáéíóúü]+(?:\s\(\d+\))?)"
                 pattern_flag = r"\b(RET|WO|DSQ)\b"
                 pattern_scores = r"(\d+|\-)\s+(\d+|\-)\s+(\d+|\-)"
                 
@@ -79,7 +82,7 @@ def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
                     team_1_names = re.findall(pattern_names, team_1, re.MULTILINE)
                     team_1_flag = re.search(pattern_flag, team_1)
                     team_1_scores = re.findall(pattern_scores, team_1, re.MULTILINE)[0]
-
+                    
                     team_2_names = re.findall(pattern_names, team_2, re.MULTILINE)
                     team_2_flag = re.search(pattern_flag, team_2)
                     team_2_scores = re.findall(pattern_scores, team_2, re.MULTILINE)[0]
@@ -105,14 +108,10 @@ def tournament_details(page: Page, tournament_dict:dict, gender_arg:str):
                                 "team_1_scores": team_1_scores, 
                                 "team_2": team_2_names, 
                                 "team_2_scores": team_2_scores, 
-                                "winner": winner_calculation(team_1_scores, team_2_scores, dict_flag)}
-                # print(match_dict)
+                                "winner": winner_calculation(date,team_1_names,team_1_scores, team_2_scores, dict_flag)}
                 match_list.append(match_dict)
                 
     return match_list        
-            # print(tournament_rounds.count
-            # rounds = [tournament_rounds.nth(i).inner_text() for i in range(tournament_rounds.count())]
-            # print(rounds)
         
 
 def scrap_padel_data(page: Page, url: str, gender_arg: str):
@@ -140,15 +139,13 @@ def scrap_padel_data(page: Page, url: str, gender_arg: str):
         tournament_dict = list_events[i]
         match_list = tournament_details(page, tournament_dict, gender_arg)
         tournament_dict["matches"] = match_list
-    # print(tournament_dict)
-    # link = event_content.locator("div.event-title").locator("a").get_attribute("href")
-    # print(link)
+
     return list_events
 
 def run(url: str, gender_arg: str):
     with sync_playwright() as playwright:
         chromium = playwright.chromium # or "firefox" or "webkit".
-        browser = chromium.launch(headless=False)
+        browser = chromium.launch(headless=True)
         page = browser.new_page()
         # page.goto("http://example.com")
         list_events = scrap_padel_data(page, url, gender_arg)
