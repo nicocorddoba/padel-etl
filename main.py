c300ba32e476f384541e0e87132ae33bc0dd91fc
Nicolas Cordoba <nico343@outlook.es>
2025-03-12 23:20:13 -0300

    fixed transformation for the FINALS Tournament. Load functions finished

--- main.py
+++ main.py
@@ -5,20 +5,34 @@ from sqlalchemy import create_engine
 
 import os
 import json
-# import pandas
-url_engine = os.environ['URL_ENGINE']
-url = "https://www.padelfip.com/es/calendario-premier-padel/?events-year="
-gender_arg = "Men" #Men or Women
-path = f'./output_scrap/{gender_arg}_events.json'
-output_transform = './output_transform'
+
+
+url_engine = os.environ['POSTGRES_URL']  # POSTGRES DB CREDENTIALS
+url = "https://www.padelfip.com/es/calendario-premier-padel/?events-year="#PAGE TO SCRAP
+
+
+gender = "Women" #Men or Women
+organization = "PP" #Premier Padel
+year = "2024"
+path_scrap = f'./output_scrap/{organization}_{gender}_{year}.json'
+path_transform = f'./output_transform/{year}_{gender}'
 if __name__ == "__main__":
-    list_events = run(url + "2024", gender_arg)
-    json.dump(list_events, open(path, "w"))
-    wins_count, prizes_sum, df_tournaments = transform(path)
-    wins_count.to_csv(f'{output_transform}/wins_count.csv', index=False)
-    prizes_sum.to_csv(f'{output_transform}/prizes_sum.csv')
-    df_tournaments.to_csv(f'{output_transform}/tournaments.csv', index=False)
+    # Scraping data
+    list_events = run(url + year, gender)
+    json.dump(list_events, open(path_scrap, "w"))
+    
+    # Transforming Data
+    matches_cleaned,tournament_winner, prizes,  tournaments = transform(path_scrap, gender)
+    
+    # Saving data in files
+    matches_cleaned.to_csv(f'{path_transform}_matches_cleaned.csv', index=False)
+    tournament_winner.to_csv(f'{path_transform}_tournament_winner.csv', index=False)
+    prizes.to_csv(f'{path_transform}_prizes_df.csv', index= False)
+    tournaments.to_csv(f'./output_transform/{organization}_tournaments.csv', index=False)
+    
+    # Data Base Engine
     eng = create_engine(url_engine)
-    table_creator('wins_count', wins_count, eng, False)
-    table_creator('prizes_sum', prizes_sum, eng)
-    table_creator('df_tournaments', df_tournaments, eng, False)
+    # Uplouding Data
+    table_creator(f'{gender.lower()}_tournament_winner', tournament_winner, eng, False)
+    table_creator(f'{gender.lower()}_prizes', prizes, eng)
+    table_creator(f'{organization.lower()}_tournaments', tournaments, eng, False)
--- plugins/transform/transform.py
+++ plugins/transform/transform.py
@@ -3,13 +3,14 @@ import json
 import re
 
 def extract_data(path: str) -> list[dict]:
+    # Data extraction from json file alocated in './output_scrap' folder
     try: 
         with open (path, encoding='utf-8') as f:
             raw_data = json.load(f)
             f.close()
 
-        matches = []
-        tournaments = []
+        matches = [] # All matches from {organization (Premier Padel)}
+        tournaments = [] # All tournaments from {organization (Premier Padel)}
         for event in raw_data:
             for match in event["matches"]:
                 match['event_title'] = event['title']
@@ -22,8 +23,24 @@ def extract_data(path: str) -> list[dict]:
         
 
 def clean_matches_df(row:pd.Series)-> pd.Series:
+    """function maded to use within an .apply from pandas.DataFrame
+    with these required columns:
+    team_1 (Array): first pair of a padel match
+    team_2 (Array): second pair of a padel match
+    winner (String): the winner team ('team_1' or 'team_2')
+    event_title (String): the title of each premier padel tournament
+    
+    Args:
+        row (pd.Series): each row from pandas.DataFrame
+
+    Returns:
+        row (pd.Series): The transformed row
+    """
     team_1 = row['team_1']
     team_2 = row['team_2']
+    # In some rows, there's the ranking number of the first 16 pairs
+    # or '(Q)' that means that pair comes from qualy rounds
+    # we are not going to use it, so we'll delete
     if len(team_1) > 0:
         if '(' in team_1[-1]:
             player_b = team_1[-1]
@@ -36,40 +53,46 @@ def clean_matches_df(row:pd.Series)-> pd.Series:
             row['team_2'] = team_2
             
     row['winner'] = row[row['winner']]
+    pattern = '(FINALS|MAJOR|P1|P2)'
+    event_category = re.search(pattern, row['event_title']).group(0)
+    row['category'] = event_category
     return row
 
 
-def prizes_calc(row: pd.Series)-> pd.Series:
-    # print(row)
+def prizes_calc(row: pd.Series, gender:str)-> pd.Series:
     player_list = row['team_1'] + row['team_2']
-    
-    prizes_dict = {'FINALS':{'winners': 52500, 'Final': 30000,'thirds': 21000, 'fourths':13500, 'Quarterfinals':7688},
-        'MAJOR': {'winners': 47250, 'Final': 23625, 'Semifinals': 13125, 'Quarterfinals':8531, 'Round of 16': 5250, 'Round of 32': 2953, 'Round of 64':1477, 'Q2': 820}, 
-        'P1': {'winners': 25500, 'Final': 13500, 'Semifinals': 7125, 'Quarterfinals':4500, 'Round of 16': 2625, 'Round of 32': 1922, 'Round of 64':1266, 'Q2': 1266}, 
-        'P2': {'winners': 15000, 'Final': 8250, 'Semifinals': 4500, 'Quarterfinals':3000, 'Round of 16': 1781, 'Round of 32': 891, 'Q3': 563 }}
-    pattern = '(FINALS|MAJOR|P1|P2)'
-    event_category = re.search(pattern, row['event_title']).group(0)
+    if gender == 'Men':
+        prizes_dict = {'FINALS':{'winners': 52500, 'Final': 30000,'thirds': 21000, 'Final 3rd place':13500, 'Quarterfinals':7688},
+            'MAJOR': {'winners': 47250, 'Final': 23625, 'Semifinals': 13125, 'Quarterfinals':8531, 'Round of 16': 5250, 'Round of 32': 2953, 'Round of 64':1477, 'Q2': 820}, 
+            'P1': {'winners': 25500, 'Final': 13500, 'Semifinals': 7125, 'Quarterfinals':4500, 'Round of 16': 2625, 'Round of 32': 1922, 'Q2': 1266}, 
+            'P2': {'winners': 15000, 'Final': 8250, 'Semifinals': 4500, 'Quarterfinals':3000, 'Round of 16': 1781, 'Round of 32': 891, 'Q3': 563 }}
+    else:
+        prizes_dict = {'FINALS':{'winners': 52500, 'Final': 30000,'thirds': 21000, 'fourths':13500, 'Quarterfinals':7688},
+            'MAJOR': {'winners': 47250, 'Final': 23625, 'Semifinals': 13125, 'Quarterfinals':8531, 'Round of 16': 5250, 'Round of 32': 2953, 'Round of 64':1477, 'Q2': 820}, 
+            'P1': {'winners': 17000, 'Final': 9350, 'Semifinals': 5100, 'Quarterfinals':3400, 'Round of 16': 2019, 'Round of 32': 1009, 'Q2': 638}, 
+            'P2': {'winners': 8500, 'Final': 4675, 'Semifinals': 2550, 'Quarterfinals':1700, 'Round of 16': 1009, 'Round of 32': 673, 'Q3': 319 }}
+        
+    event_category = row['category']
     round_name = row['round_name']
-    list_of_rows = []
+    row_list = []
     if round_name in prizes_dict[event_category].keys():
         for player in player_list:
             if player not in row['winner']: 
-                if event_category != 'FINALS':
+                if event_category != 'FINALS' or round_name != 'Final 3rd place':
                     prize = prizes_dict[event_category][round_name]
-                    list_of_rows.append({'player': player, 'prize':prize})
+                    row_list.append({'player': player, 'prize':prize, 'tournament':row['event_title'], 'category': event_category})
                 else: 
-                    prize = prizes_dict[event_category]['fourths']
-                    list_of_rows.append({'player': player, 'prize':prize})
-            
+                    prize = prizes_dict[event_category]['Final 3rd place']
+                    row_list.append({'player': player, 'prize':prize, 'tournament':row['event_title'], 'category': event_category})
             else:
                 if round_name == 'Final':
                     prize = prizes_dict[event_category]['winners']
-                    list_of_rows.append({'player': player, 'prize':prize})
-                    
+                    row_list.append({'player': player, 'prize':prize, 'tournament':row['event_title'], 'category': event_category})
                 elif round_name == 'Final 3rd place':
-                    prize = prizes_dict[event_category]['thirds']
-                    list_of_rows.append({'player': player, 'prize':prize})
-        df_to_return = pd.DataFrame(list_of_rows)
+                    prize = prizes_dict[event_category]['thirds']       
+                    row_list.append({'player': player, 'prize':prize, 'tournament':row['event_title'], 'category': event_category})
+
+        df_to_return = pd.DataFrame(row_list)
         return df_to_return
     else: return None
 
@@ -81,20 +104,19 @@ def loc_split(row):
     return pd.Series(index=('city', 'country', 'category'), data=(city, country, category))
 
 
-def transform(path: str):
-    path = './output_scrap/Men_events.json'
-    matches, tournaments = extract_data(path)
+def transform(path_scrap: str, gender:str):
+    matches, tournaments = extract_data(path_scrap)
     df_matches = pd.DataFrame(matches)
     df_matches_cleaned = df_matches.apply(axis=1, func=clean_matches_df)
     df_final_round = df_matches_cleaned.copy()[df_matches_cleaned['round_name'] == 'Final']
     df_final_round['winner'] = df_final_round.loc[:,'winner'].map(lambda x: ', '.join(x))
-    wins_count = df_final_round.groupby('winner')['winner'].count().reset_index(name='counts')
-    
-    prizes_df = pd.concat(df_matches_cleaned.apply(axis=1, func=prizes_calc).dropna().to_list(), ignore_index=True)
-    prizes_sum = prizes_df.groupby('player').sum('prize').sort_values(by= 'prize',ascending=False)
+
+    winners = df_final_round[['winner', 'event_title', 'category']]
+    prizes_df = pd.concat(df_matches_cleaned.apply(axis=1, func=prizes_calc, gender=gender).dropna().to_list(), ignore_index=True)
+    # prizes_sum = prizes_df.groupby('player').sum('prize').sort_values(by= 'prize',ascending=False)
     
     
     df_tournaments = pd.DataFrame(tournaments)
     df_tournaments[['city', 'country', 'category']] = df_tournaments.apply(func=loc_split, axis=1)
     
-    return (wins_count, prizes_sum, df_tournaments)
+    return (df_matches_cleaned,winners, prizes_df, df_tournaments)
